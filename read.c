fb778bf839a388d747723cfa762b00740b420852
BrayoFsc <bryan.fellipe.sc@gmail.com>
2020-09-15 01:55:31 -0300

    simplifiquei a leitura e adicionei o tratamento de parametros

--- read.c
+++ read.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <err.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -5,6 +6,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 typedef struct WAVE {
    //[CHUNK RIFF]
@@ -27,66 +29,41 @@ typedef struct WAVE {
    uint32_t samplesPC; // Number of Samples per Channel
 } wave;
 
-int main()
+int main(int argc, char **argv)
 {
-
+   char *fileIn = NULL, *fileOut = NULL;
    FILE *wav;
-   char *filesrc;
+   int option;
    wave audio;
-   uint16_t buffer2[2];
-   filesrc = "music.wav";
+   if (argc > 1)
+   {
+      while ((option = getopt(argc, argv, "i:o:")) != -1)
+      {
+         switch (option)
+         {
+         case 'i':
+            fileIn = optarg;
+            break;
+
+         case 'o':
+            fileOut = optarg;
+            break;
+         }
+      }
+   }
+
    printf("Opening file..\n");
-   wav = fopen(filesrc, "rb");
+   if (fileIn == NULL)
+      wav = stdin;
+   else
+      wav = fopen(fileIn, "rd");
+
    if (wav == NULL)
    {
       printf("Error opening filen");
       exit(1);
    }
-   // read RIFF parts
-   fread(&audio.chunkID, 1, 4, wav);
-   printf("( 1- 4) ");
-   for (int i = 0; i < 4; i++) printf("%c", audio.chunkID[i]);
-   printf("\n");
 
-   fread(&audio.chunkSize, 4, 1, wav);
-   printf("( 5- 8)  Chunk Size: %u bytes %u Kb\n", audio.chunkSize, audio.chunkSize / 1024);
-
-   fread(&audio.format, 4, 1, wav);
-   printf("( 9-12) File format: %s\n", audio.format);
-   // read FMT parts
-   fread(&audio.fmtChunkID, 1, 4, wav);
-   printf("(13-16) FMT Chunk Header: %s\n", audio.fmtChunkID);
-
-   fread(&audio.fmtChunkSize, 4, 1, wav);
-   printf("(17-20) FMT CHunk Size: %u\n", audio.fmtChunkSize);
-
-   fread(&audio.audioFormat, 2, 1, wav);
-   printf("(21-22) Audio Format: %u\n", audio.audioFormat);
-
-   // fread(&buffer2, 2, 1, wav);
-   // audio.channels = buffer2[0] | (buffer2[1]);
-   fread(&audio.channels, 2, 1, wav);
-   printf("(23-24) Number of Channels: %u\n", audio.channels);
-
-   fread(&audio.sampleRate, 4, 1, wav);
-   printf("(25-28) Sample Rate: %u\n", audio.sampleRate);
-
-   fread(&audio.byteRate, 4, 1, wav);
-   printf("(29-32) Byte Rate: %u\n", audio.byteRate);
-
-   fread(&audio.blockAlign, 2, 1, wav);
-   printf("(33-34) Block Align: %u\n", audio.blockAlign);
-
-   fread(&audio.bitsPS, 2, 1, wav);
-   printf("(35-36) Bits Per Sample: %u\n", audio.bitsPS);
-
-   fread(&audio.dataChunkID, 1, 4, wav);
-   printf("(37-40) ");
-   for (int i = 0; i < 4; i++) printf("%c", audio.dataChunkID[i]);
-   printf("\n");
-
-   fread(&audio.dataChunkSize, 4, 1, wav);
-   printf("(41-44) Data Size: %u\n", audio.dataChunkSize);
+   fread(&audio, 1, 44, wav);
    audio.samplesPC = (audio.dataChunkSize / audio.channels / (audio.bitsPS / 8));
-   printf("%u\n", audio.samplesPC);
 }
